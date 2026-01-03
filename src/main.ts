0f1baeeb7754fdee87e1ca812523d748100f0018
Hipolite Oleru <lite0012@gmail.com>
2026-01-03 15:19:07 -0500

    Log real exceptions for 500s

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,11 @@ import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // IMPORTANT: register filters BEFORE listen()
   app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
-
 bootstrap();
+
 
 
