408981871af7317ff2033c33d13b4faf99db96df
Hipolite Oleru <lite0012@gmail.com>
2026-01-05 11:00:13 -0500

    Fix bootstrap + global exception filter

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // MUST be before listen()
+  // must be before listen()
   app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
+
 
 
 
